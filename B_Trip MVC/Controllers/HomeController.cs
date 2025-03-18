df81a079ac5aa613f33aed9f34d6e0c4b510235a
Abdallah Bahaa <abdallahbahaa.200465@gmail.com>
2025-03-18 16:15:29 +0200

    BTRIP-Project

---
+++ B_Trip MVC/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Mvc;
+
+namespace B_Trip_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+      
+        public IActionResult Recent()
+        {
+            return View();
+        }
+    }
+}
--- B_Trip MVC/Program.cs
+++ B_Trip MVC/Program.cs
@@ -5,9 +5,21 @@ namespace B_Trip_MVC
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            app.UseRouting();
+
+            app.UseStaticFiles();
+
+
+
+            //app.MapGet("/", () => "Hello World!");
+            app.MapControllerRoute(
+                name: "Default",
+                pattern: "{controller=Home}/{action=Index}"
+                );
+
 
             app.Run();
         }
